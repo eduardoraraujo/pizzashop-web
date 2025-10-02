849649b4bfbc7b76b812dc07b5c366fc1accba9c
Eduardo Ronchi de Araujo <eduardoaraujo2609@gmail.com>
2025-10-01 21:57:48 -0300

    feat: update app structure with new layouts, forms, and styling
    - Added global CSS file for consistent styling across the app.
    - Introduced AppLayout and AuthLayout for better route management.
    - Implemented SignIn and SignUp pages with form validation using Zod.
    - Integrated react-helmet-async for dynamic document title management.
    - Updated button and input components for improved UI consistency.
    - Added favicon and global styles for better user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster
+        richColors
+        closeButton
+        position="top-right"
+        toastOptions={{
+          classNames: {
+            toast: '!gap-2',
+            actionButton: '!p-3',
+          },
+        }}
+      />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
