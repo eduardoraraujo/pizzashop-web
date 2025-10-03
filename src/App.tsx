5fb70fdd700835c8417cee419dbe9b9ae70847f1
Eduardo Ronchi de Araujo <eduardoaraujo2609@gmail.com>
2025-10-02 22:14:54 -0300

    feat: add theme provider and toggle, enhance header and account menu
    - Introduced ThemeProvider to manage light/dark themes.
    - Added ThemeToggle component for users to switch themes.
    - Enhanced Header component with navigation links and account menu.
    - Created AccountMenu for user profile actions.
    - Updated AppLayout and AuthLayout for consistent styling.
    - Implemented Orders page with a table for displaying order details.
    - Refactored button styles for better accessibility and usability.
    - Added dropdown menu components for better UI interactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster
-        richColors
-        closeButton
-        position="top-right"
-        toastOptions={{
-          classNames: {
-            toast: '!gap-2',
-            actionButton: '!p-3',
-          },
-        }}
-      />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster
+          richColors
+          closeButton
+          position="top-right"
+          toastOptions={{
+            classNames: {
+              toast: '!gap-2',
+              actionButton: '!p-3',
+            },
+          }}
+        />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
---
+++ src/components/account-menu.tsx
@@ -0,0 +1,43 @@
+import { Building2, ChevronDown, LogOut } from 'lucide-react'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export default function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Eduardo Ronchi</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            eduardo.ronchi@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building2 className="w-4 h-4 mr-2" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem variant="destructive">
+          <LogOut className="w-4 h-4 mr-2 " />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
